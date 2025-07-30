394993ffd25dd21846b54e38534fc67c80b09b79
Misha Korotia <mr.anderson528491@gmail.com>
2025-07-30 21:51:31 +0300

    implemented parser for pdf file

--- pdf_summary/constraints.py
+++ pdf_summary/constraints.py
@@ -1,8 +1,14 @@
+import os
+import fitz
 from fastapi import UploadFile
 from fastapi import HTTPException
 from starlette import status
 
 FILE_SIZE = 50 * 1024 * 1024
+
+UPLOAD_DIR = "uploads"  # 50 MB file
+
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
 def check_file_size(file: UploadFile):
@@ -22,4 +28,44 @@ def check_file_type(file: UploadFile):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Only PDF files are allowed"
+        )
+
+
+def check_file_exists(file: str):
+    if not os.path.exists(file):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="File not found"
+        )
+
+
+def parse_pdf_file(filename: str):
+    file_path = os.path.join(UPLOAD_DIR, filename)
+
+    try:
+        doc = fitz.open(file_path)
+        parsed_text = ""
+        image_count = 0
+
+        for page in doc:
+            text = page.get_text()
+            parsed_text += text
+
+            images = page.get_images(full=True)
+            image_count += len(images)
+
+        number_of_pages = len(doc)
+        doc.close()
+
+        return {
+            "text_preview": parsed_text[:100],
+            "full_text": parsed_text,
+            "number_of_images": image_count,
+            "pages": number_of_pages
+        }
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error occured during parsing file. Error:{e}"
         )
