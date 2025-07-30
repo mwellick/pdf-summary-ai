6da7d6057792d950bdb116015184ca3e5271feb4
Misha Korotia <mr.anderson528491@gmail.com>
2025-07-30 21:52:04 +0300

    Implemented parse_pdf endpoint

--- pdf_summary/routers.py
+++ pdf_summary/routers.py
@@ -1,8 +1,8 @@
-import fitz
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from starlette import status
 
 from .crud import upload_pdf_file
+from .constraints import parse_pdf_file
 from dependencies import db_dependency
 
 pdf_router = APIRouter()
@@ -11,3 +11,8 @@ pdf_router = APIRouter()
 @pdf_router.post("/upload", status_code=status.HTTP_201_CREATED)
 def upload_pdf(file: UploadFile = File(...)):
     return upload_pdf_file(file)
+
+
+@pdf_router.get("/parse", status_code=status.HTTP_200_OK)
+def parse_pdf(filename: str):
+    return parse_pdf_file(filename)
