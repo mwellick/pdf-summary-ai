050cb0ae5d38b576b2ea4576ca3d7639120747bc
Misha Korotia <mr.anderson528491@gmail.com>
2025-07-31 00:00:16 +0300

    Created get ai summary endpoint

--- pdf_summary/routers.py
+++ pdf_summary/routers.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from starlette import status
-
-from .crud import upload_pdf_file
+from .crud import upload_pdf_file, save_summary_to_db
 from .constraints import parse_pdf_file
 from dependencies import db_dependency
 
@@ -16,3 +15,8 @@ def upload_pdf(file: UploadFile = File(...)):
 @pdf_router.get("/parse", status_code=status.HTTP_200_OK)
 def parse_pdf(filename: str):
     return parse_pdf_file(filename)
+
+
+@pdf_router.get("/summary", status_code=status.HTTP_200_OK)
+def get_ai_summary(filename: str, db: db_dependency):
+    return save_summary_to_db(filename, db)
