9c1d5b7d019d02e6daceb3ee3bc718502855cf4c
Misha Korotia <mr.anderson528491@gmail.com>
2025-07-30 23:56:52 +0300

    Updated database.py file

--- database/database.py
+++ database/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"chack_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 
