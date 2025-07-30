2000b57829400af88bc6e06de1593bf6586bbe55
Misha Korotia <mr.anderson528491@gmail.com>
2025-07-31 00:09:15 +0300

    Created get last summaries endpoint

--- pdf_summary/routers.py
+++ pdf_summary/routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from starlette import status
-from .crud import upload_pdf_file, save_summary_to_db
+from .crud import upload_pdf_file, save_summary_to_db, get_last_five_docs
 from .constraints import parse_pdf_file
 from dependencies import db_dependency
 
@@ -20,3 +20,8 @@ def parse_pdf(filename: str):
 @pdf_router.get("/summary", status_code=status.HTTP_200_OK)
 def get_ai_summary(filename: str, db: db_dependency):
     return save_summary_to_db(filename, db)
+
+
+@pdf_router.get("/summaries", status_code=status.HTTP_200_OK)
+def get_last_five_summaries(db: db_dependency):
+    return get_last_five_docs(db)
