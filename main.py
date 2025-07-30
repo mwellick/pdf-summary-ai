885ed34291702cc2edeebba1bc851a23f4f23b67
Misha Korotia <mr.anderson528491@gmail.com>
2025-07-31 00:14:54 +0300

    updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
+from pdf_summary.routers import pdf_router
 
 app = FastAPI(title="PDF Summary AI")
 
-
-@app.get("/")
-def get_root():
-    return "Hello world"
+app.include_router(pdf_router)
